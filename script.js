4c3c47d0047038a0fb009344e5060399a2e2c9fd
Sadia Naaz <139625425+Sadia-Naaz@users.noreply.github.com>
2023-11-21 08:03:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 var isDate = function (input) {
   //   write your code here
+if (Object.prototype.toString.call(input)==='[Object Date]'){
+	return !isNaN(input.getTime());
+}else if(typeof input==='string'){
+	const date = new Date(input);
+	return !isNaN(input.getTime());
+}else{
+	return false;
+}
 };
 
-// Do not change the code below.
+Do not change the code below.
 const input = prompt("Enter Date.");
 alert(isDate(input));
